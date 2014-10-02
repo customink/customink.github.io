bcef8d08c83558e1032756d0e1a7a499e48feec7
Jonathan Lehman <jonathan.lehman91@gmail.com>
2014-10-02 14:53:49 -0400

    Add Rake tasks to handle tech blog iconfonts
    - Use Rake tasks to load/update icon fonts via fontello. This makes the
      task much easier than moving/editing the files manually.
    
    From: 1055edff61033f2f17eb8c90fe6b230e936ea371

--- bower_components/style_bitz/app/assets/javascripts/style_bitz.js
+++
@@ -1,3 +0,0 @@
-//= require jquery
-//= require jquery-cookie
-//= require style_bitz/core
--- bower_components/style_bitz/lib/style_bitz/rails.rb
+++
@@ -1,9 +0,0 @@
-require 'rails'
-require 'rails/engine'
-require 'style_bitz/rails/engine'
-
-module StyleBitz
-  module Rails
-
-  end
-end
